2a7010c98225f332aff24b387a4f76f73329e648
Marjorie Kehil <marjorie.kehil28@hotmail.com>
2023-09-28 23:05:50 +0200

    corrections data

--- components/data.js
+++ components/data.js
@@ -5,7 +5,7 @@ export const projectData = [
       id: 1,
       title: "Plant'Api",
       description:
-        "Une appli web destinée aux passionnés d'apiculture pour explorer le monde fascinant des plantes mellifères. Construit avec Next.js, API REST Node.js, ORM Prisma et TaiWindCSS. ",
+        "Une appli web destinée aux passionnés d'apiculture pour explorer le monde fascinant des plantes mellifères. Construit avec Next.js, API REST Node.js, PosgreSQL (Supabase), ORM Prisma et TaiWindCSS. ",
       image : "/image/plantapi.jpg",
       gitHubLink: '/',
       appLink: 'https://plant-api.vercel.app/',
@@ -14,10 +14,10 @@ export const projectData = [
       id: 2,
       title: 'Cuisine en Famille',
       description:
-        'Une application web destinée à un usage familial pour partager et découvrir les recettes de chacun. Réalisé en PHP, Bootstrap.',
+        'Une application web destinée à un usage familial pour partager et découvrir les recettes de chacun. Contruit avec React, API REST Node.js, mySQL et Bootstrap.',
       image : "/image/cuisine.jpg",
       gitHubLink: '/',
-      appLink: 'https://plant-api.vercel.app/',
+      appLink: 'https://pixie-s-cooking.vercel.app/',
     },
     {
       id: 3,
@@ -32,9 +32,9 @@ export const projectData = [
       id: 4,
       title: 'BookFlix',
       description:
-        "Une appli web de bibliothèque de films, livres et musiques. Construit avec React, API REST Node.js et Bootstrap.",
+        "Une appli web de bibliothèque de films, livres et musiques. Construit avec React, API REST Node.js, MongoDB et Bootstrap.",
       image : "/image/bookflix.jpg",
-      gitHubLink: '/',
+      gitHubLink: 'https://github.com/Zwina/Bookflix',
       appLink: 'https://bookflix-zwina.vercel.app/',
     },
 
